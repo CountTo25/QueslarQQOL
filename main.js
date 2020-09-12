c60e0efe874dfe2229fdfd390544f983c2c25186
CountTo25 <FiammaTheGreat@ya.ru>
2020-09-12 04:23:12 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 class FTGMod {
  constructor() {
-   let el
+   let timetoleveluptooltip = document.createElement('div');
+   timetoleveluptooltip.id = 'FTG_time_to_levelup';
+   document.getElementById('profile-next-level').parentNode.insertBefore(timetoleveluptooltip,document.getElementById('profile-next-level').nextSibling)
+
    this.update = setInterval(this.Update.bind(this),1000);
    this.Update();
-   console.log('loaded Quality of Quality of Life mod v0.01. Have a nice day!');
+   console.log('loaded Quality of Quality of Life mod v0.02. Have a nice day!');
+
+
 
  }
 
  Update() {
    this.TimeRemaining();
+   this.TimeToLevelUp();
  }
 
  CreateTimerWindow() {
@@ -32,6 +38,21 @@ class FTGMod {
    txt = hourval+':'+(remMinutes<10?('0'+remMinutes):(remMinutes));
    document.getElementById('FTG_idle_timer').innerHTML=('Idle Time Remaining: ' + txt);
    console.log(txt);
+ }
+
+ TimeToLevelUp() {
+   let txt = document.getElementById('profile-next-level').innerHTML;
+   let actionVal = parseInt(txt.replace(/\D/g,''));
+   txt='('+this.ActionsToTime(actionVal)+')';
+   document.getElementById('FTG_time_to_levelup').innerHTML = txt;
+
+ }
+
+ActionsToTime(actions) {
+   let minval = Math.floor(actions/10);
+   let hourval = Math.floor(minval/60);
+   let remMinutes = minval-hourval*60;
+   return hourval+':'+(remMinutes<10?('0'+remMinutes):(remMinutes));
  }
 }
 
